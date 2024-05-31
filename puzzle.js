b96c99d64360fd51232d424c0ff454f1fc6c85c9
BP--- <bp_114@hotmail.com>
2024-05-31 16:02:13 +0200

    added logic to get all the solutions

--- puzzle.js
+++ puzzle.js
@@ -1,6 +1,18 @@
 
 
 function puzzle() {
-    return "test";
+    const alternatives = ["Basilisk", "Ravn", "Orm", "Skarabe", "Rev", "Ulv", "Bjørn", "Fønix"];
+    const solutions = [];
+    for (let i=0; i<alternatives.length; i++) {
+        for (let j = 0; j<alternatives.length; j++) {
+            for (let k = 0; k<alternatives.length; k++) {
+                for(let l = 0; l<alternatives.length; l++){
+                    solutions.push([alternatives[i], alternatives[j], alternatives[k], alternatives[l]]);
+                }
+            }
+        }
+    }
+    
+    return solutions;
     
 }
